5df5ea1005b526e20f75a230fefbbd846c1b0310
RJIOSDEV <97068296+RJIOSDEV@users.noreply.github.com>
2025-07-13 13:23:22 +0530

    Update ToastModel.swift

--- Sources/ToastifySwift/ToastModel.swift
+++ Sources/ToastifySwift/ToastModel.swift
@@ -1,10 +1,21 @@
-
 import SwiftUI
 
 public struct ToastModel: Identifiable {
     public var id = UUID()
     public var message: String
-    public var icon: String = "info.circle"
-    public var backgroundColor: Color = .black
-    public var duration: TimeInterval = 2.0
+    public var icon: String
+    public var backgroundColor: Color
+    public var duration: TimeInterval
+
+    public init(
+        message: String,
+        icon: String = "info.circle",
+        backgroundColor: Color = .black,
+        duration: TimeInterval = 2.0
+    ) {
+        self.message = message
+        self.icon = icon
+        self.backgroundColor = backgroundColor
+        self.duration = duration
+    }
 }
